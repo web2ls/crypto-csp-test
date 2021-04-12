c4e938f79d2ffb41cfa27ec449bf947cf4d3f245
web2ls <web2ls@yandex.ru>
2021-04-12 09:51:40 +0300

    basic app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useEffect } from 'react';
+import { createDetachedSignature, createHash } from 'crypto-pro';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const signIt = async () => {
+    const message = 'Hello Workld';
+    try {
+      const messageHash = await createHash(message);
+
+      try {
+        const signature = await createDetachedSignature('certname', messageHash);
+        console.log(signature);
+      } catch (signatureError) {
+        console.log(signatureError);
+      }
+    } catch (hashError) {
+      console.log(hashError);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" onClick={signIt}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
